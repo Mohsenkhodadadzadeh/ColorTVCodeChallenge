e290c302f48a7ebc0bea968dcde79c674658ad4a
Mohsen khodadadazadeh <mohsen@nobody.invalid>
2020-07-18 12:32:24 +0430

    init

---
+++ ColorTVCodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorTVCodeChallenge
+//
+//  Created by Mohsen khodadadazadeh on 7/18/20.
+//  Copyright © 2020 Mohsen khodadadazadeh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-ColorTVCodeChallenge/Pods-ColorTVCodeChallenge-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ColorTVCodeChallenge : NSObject
+@end
+@implementation PodsDummy_Pods_ColorTVCodeChallenge
+@end
